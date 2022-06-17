9c99e7890898c4ce91ed7d8fc426fc65025b4bb0
Aidan Antony <aidanantony65@gmail.com>
2022-06-17 09:38:07 -0600

    First queries commit

---
+++ 1_queries/most_visited_cities.sql
@@ -0,0 +1,4 @@
+SELECT properties.city AS city, count(reservations.*) as total_reservations
+FROM properties JOIN reservations ON properties.id = property_id
+GROUP BY properties.city
+ORDER BY total_reservations DESC;
